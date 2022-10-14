b2c247292def025e596fdea12cf1d888274bde69
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-10-14 18:49:16 -0400

    Aplicando herencia en JS y Python

---
+++ JS/UberBlack.js
@@ -0,0 +1,8 @@
+class UberBlack extends Car {
+    
+    constructor(license, driver, typerCarAccepted, seatMaterial){
+        super(license, driver)
+        this.typerCarAccepted = typerCarAccepted
+        this.seatMaterial = seatMaterial
+    }
+}
